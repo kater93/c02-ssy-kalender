a6c76439ab5da25d873c82ae686d71d261a2381c
Arno Hollosi <arno.hollosi@campus02.at>
2018-12-21 15:19:11 +0100

    Templating-Engine entfernt, Package-Updates

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Kalender 4.0' });
+  res.send('NodeJS + Express läuft!');
 });
 
 module.exports = router;
